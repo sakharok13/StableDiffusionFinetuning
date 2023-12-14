91b1de74ec3969d35fce7e46a59e96afbf9bd276
Alexander Gambashidze <amgambashidze@edu.hse.ru>
2023-12-14 11:53:48 +0300

    training ipynb

--- data/download_images.py
+++ data/download_images.py
@@ -29,7 +29,7 @@ def main():
     parser = argparse.ArgumentParser(description='Write the number of images to download')
     parser.add_argument('--img_count', type=int, default=10000, required=False, help='an integer for the accumulator')
     parser.add_argument('--parquet_path', type=str, default='./laion-pop/laion_pop.parquet', required=False , help='Path to laion-pop parquet')
-    parser.add_argument('--workers', type=int, default=5, required=False , help='Number of cpus involved')
+    parser.add_argument('--workers', type=int, default=32, required=False , help='Number of cpus involved')
     args = parser.parse_args()
     
     df = pd.read_parquet(args.parquet_path)
@@ -37,12 +37,12 @@ def main():
     image_dir = 'images'
     os.makedirs(image_dir, exist_ok=True)
     
-    data_to_download = df[:args.img_count]
+    data_to_download = df[6900:args.img_count]
     data_to_download = [(url, key) for url, key in zip(data_to_download['url'], data_to_download['key'])]
     del df
     
     with Pool(args.workers) as pool:
-        list(tqdm(pool.imap_unordered(download_image, data_to_download), total=args.img_count))
+        list(tqdm(pool.imap_unordered(download_image, data_to_download), total=args.img_count-6900))
     
 if __name__ == '__main__':
     main()
